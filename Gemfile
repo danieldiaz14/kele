7b6f07ff2c846203cdaf53398ffbe93b493e0964
Daniel Diaz Medrano <danieldiaz129@gmail.com>
2017-08-01 19:47:48 +0000

    3 Retrieve Users

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in kele.gemspec
 gemspec
-gem 'httparty', '~> 0.13'
+gem 'httparty', '~> 0.15.6'
+gem 'json', '~> 1.8', '>= 1.8.6'
--- lib/kele.rb
+++ lib/kele.rb
@@ -1,6 +1,7 @@
 require "kele/version"
 require './lib/kele'
 require 'httparty'
+require 'json'
 
 class Kele
     include HTTParty
@@ -8,5 +9,10 @@ class Kele
     def initialize(email, password)
         response = self.class.post(api_url('sessions'), body: { "email": email, "password": password })
         @auth_token = response["auth_token"]
+    end
+    
+    def get_me
+        response = self.class.get(api_url('users/me'), headers: { "authorization" => @auth_token })
+        @user_data = JSON.parse(response.body)
     end
 end
