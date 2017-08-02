2ff9b2d61fb678344b9d1a346e08cf6fe38960a2
Daniel Diaz Medrano <danieldiaz129@gmail.com>
2017-08-02 02:41:18 +0000

    4 Mentor Availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -11,8 +11,23 @@ class Kele
         @auth_token = response["auth_token"]
     end
     
+    def api_url(url)
+        "https://www.bloc.io/api/v1/#{url}"
+    end
+    
     def get_me
         response = self.class.get(api_url('users/me'), headers: { "authorization" => @auth_token })
         @user_data = JSON.parse(response.body)
+    end
+    
+    def get_mentor_availability 
+        response = self.class.get(api_url("mentors/#{current_enrollment['mentor_id']}/student_availability"), user_auth)
+        @mentor_availability = JSON.parse(response.body)
+    end
+    
+    def user_auth
+        {headers: {
+            authorization: @auth_token
+        }}
     end
 end
