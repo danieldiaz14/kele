f5fb7d7204b902af5d666809ce44cfe2d70ac5ee
Daniel Diaz Medrano <danieldiaz129@gmail.com>
2017-08-02 04:13:15 +0000

    6 Messaging

--- lib/kele.rb
+++ lib/kele.rb
@@ -22,6 +22,19 @@ class Kele
         @user_data = JSON.parse(response.body)
     end
     
+    def create_message(user_id, recipient_id, token, subject, stripped)
+        options = {body: {user_id: user_id, recipient_id: recipient_id, token: nil, subject: subject, stripped: stripped}, headers: { "authorization" => @auth_token }}
+        self.class.post(api_url("messages"), options)
+    end
+    
+    def get_messages(arg = nil)
+        response = self.class.get(api_url("message_threads"), headers: { "authorization" => @auth_token })
+        body = JSON.parse(response.body)
+        pages = (1..(response["count"]/10 + 1)).map do |n|
+            self.class.get(api_url("message_threads"), body: { page: n }, headers: { "authorization" => @auth_token })
+        end
+    end
+    
     def get_mentor_availability 
         response = self.class.get(api_url("mentors/#{current_enrollment['mentor_id']}/student_availability"), user_auth)
         @mentor_availability = JSON.parse(response.body)
